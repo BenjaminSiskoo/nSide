d5ce53d943c0196a9484cd3669b4ddd6105d39bb
hex-usr <hex-usr@users.noreply.github.com>
2015-04-03 18:02:08 -0700

    Famicom Disk System board rearrangement
    Rearranged some Famicom Disk System code and added IRQs and wavetable
    RAM.

--- fc/cartridge/board/fds.cpp
+++ fc/cartridge/board/fds.cpp
@@ -1,18 +1,77 @@
 struct FDS : Board {
 
-uint16 irq_timer = 0x0000;
-uint8 fds_control = 0x2E;
-bool irq_enable = false;
-uint2 io_enable = 0b00;
+uint16 irq_counter;
+uint16 irq_latch;
+bool irq_repeat;
+bool irq_enable;
+
+bool disk_io_enable;
+bool sound_io_enable;
+
+uint8 fds_control;
+bool mirror;
+
+bool irq_pending;
+bool byte_transferred;
+
+uint8 data_register;
+
+bool disk_slot_empty;
+bool disk_not_ready;
+bool disk_write_protected;
+
+uint6 wavetable[0x40];
+bool wavetable_write_enable;
+
+void enter() {
+  while(true) {
+    if(scheduler.sync == Scheduler::SynchronizeMode::All) {
+      scheduler.exit(Scheduler::ExitReason::SynchronizeEvent);
+    }
+
+    if(irq_enable && irq_counter > 0 && --irq_counter == 0) {
+      cpu.set_irq_line(1);
+      if(irq_repeat) irq_counter = irq_latch;
+      else           irq_enable = false;
+      irq_latch = 0; // for Kaettekita Mario Bros.?
+    } else {
+      //TODO: Drive timer emulation
+    }
+    tick();
+  }
+}
 
 uint8 prg_read(unsigned addr) {
   switch(addr) {
+  case 0x4030:
+    {
+      uint8 data = (irq_pending | (byte_transferred << 1));
+      irq_pending = false;
+      byte_transferred = false;
+      // 0x10: CRC 0=pass; 1=fail
+      // 0x40: End of disk head
+      // 0x80: Disk read/write enable
+      cpu.set_irq_line(0);
+      return 0;
+    }
+  case 0x4031:
+    return data_register;
   case 0x4032:
-    // 0x01: 0=Disk inserted;       1=Disk ejected
-    // 0x02: 0=Disk ready;          1=Disk not ready
-    // 0x04: 0=Not write protected; 1=Write protected (on if disk ejected)
-    return 0x07;
-  case 0x4033: return 0x00; // 7-bit expansion input; 0x80 means low battery
+    return (
+      (disk_slot_empty      << 0) |
+      (disk_not_ready       << 1) |
+      (disk_write_protected << 2)
+    );
+  case 0x4033:
+    // 7-bit expansion input; 0x80 means low battery
+    return 0x00;
+  case 0x4090:
+    return cpu.mdr() & 0xc0;
+  case 0x4092:
+    return cpu.mdr() & 0xc0;
+  }
+  if((addr & 0xffc0) == 0x4040) {
+    return wavetable[addr & 0x3f] | (cpu.mdr() & 0xc0);
   }
   switch(addr & 0xe000) {
   case 0x6000:
@@ -26,23 +85,57 @@ uint8 prg_read(unsigned addr) {
 
 void prg_write(unsigned addr, uint8 data) {
   switch(addr) {
-  case 0x4020: irq_timer &= 0xff00 | data;
-  case 0x4021: irq_timer &= 0x00ff | (data << 8);
-  case 0x4022: irq_enable = data & 0x02;
-  case 0x4023: io_enable = data & 0x03;
-  case 0x4025: fds_control = data;
+  case 0x4020: irq_latch = (irq_latch & 0xff00) | (data << 0); break;
+  case 0x4021: irq_latch = (irq_latch & 0x00ff) | (data << 8); break;
+  case 0x4022:
+    irq_repeat = data & 0x01;
+    irq_enable = data & 0x02;
+    irq_counter = irq_latch;
+    byte_transferred = false;
+    cpu.set_irq_line(0); // if pending IRQ flag is clear
+    break;
+  case 0x4023:
+    disk_io_enable = data & 0x01;
+    sound_io_enable = data & 0x02;
+    break;
+  case 0x4024:
+    // clear pending IRQ flag
+    if(!byte_transferred) cpu.set_irq_line(0);
+    break;
+  case 0x4025:
+    fds_control = data & 0xf7;
+    mirror = data & 0x08;
+    break;
+  case 0x4026:
+    // 7-bit expansion output
+    break;
+  case 0x4080: break;
+  case 0x4082: break;
+  case 0x4083: break;
+  case 0x4084: break;
+  case 0x4085: break;
+  case 0x4086: break;
+  case 0x4087: break;
+  case 0x4088: break;
+  case 0x4089:
+    wavetable_write_enable = data & 0x80;
+    break;
+  case 0x408a: break;
+  }
+  if((addr & 0xffc0) == 0x4040) {
+    if(wavetable_write_enable) wavetable[addr & 0x3f] = data & 0x3f;
   }
   switch(addr & 0xe000) {
   case 0x6000:
   case 0x8000:
   case 0xa000:
-  case 0xc000: write(prgram, addr,data); break;
+  case 0xc000: write(prgram, addr, data); break;
   }
 }
 
 uint8 chr_read(unsigned addr) {
   if(addr & 0x2000) {
-    if(fds_control & 0x08) addr = ((addr & 0x0800) >> 1) | (addr & 0x03ff);
+    if(mirror) addr = ((addr & 0x0800) >> 1) | (addr & 0x03ff);
     return ppu.ciram_read(addr);
   }
   return Board::chr_read(addr);
@@ -50,14 +143,66 @@ uint8 chr_read(unsigned addr) {
 
 void chr_write(unsigned addr, uint8 data) {
   if(addr & 0x2000) {
-    if(fds_control & 0x08) addr = ((addr & 0x0800) >> 1) | (addr & 0x03ff);
+    if(mirror) addr = ((addr & 0x0800) >> 1) | (addr & 0x03ff);
     return ppu.ciram_write(addr, data);
   }
   return Board::chr_write(addr, data);
 }
 
+void power() {
+  reset();
+}
+
+void reset() {
+  irq_counter = 0;
+  irq_latch = 0;
+  irq_repeat = false;
+  irq_enable = false;
+
+  disk_io_enable = false;
+  sound_io_enable = false;
+
+  fds_control = 0x26;
+  mirror = false;
+
+  irq_pending = false;
+  byte_transferred = false;
+
+  data_register = 0x00;
+
+  disk_slot_empty = true;
+  disk_not_ready = true;
+  disk_write_protected = true;
+
+  for(auto& n : wavetable) n = 0x00;
+  wavetable_write_enable = false;
+}
+
 void serialize(serializer& s) {
   Board::serialize(s);
+
+  s.integer(irq_counter);
+  s.integer(irq_latch);
+  s.integer(irq_repeat);
+  s.integer(irq_enable);
+
+  s.integer(disk_io_enable);
+  s.integer(sound_io_enable);
+
+  s.integer(fds_control);
+  s.integer(mirror);
+
+  s.integer(irq_pending);
+  s.integer(byte_transferred);
+
+  s.integer(data_register);
+
+  s.integer(disk_slot_empty);
+  s.integer(disk_not_ready);
+  s.integer(disk_write_protected);
+
+  s.array(wavetable);
+  s.integer(wavetable_write_enable);
 }
 
 FDS(Markup::Node& cartridge) : Board(cartridge) {
