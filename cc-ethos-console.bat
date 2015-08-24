382002a3a8a5fc1671763af9aa8d983b669ee86f
hex-usr <hex-usr@users.noreply.github.com>
2015-08-24 12:45:37 -0700

    Rename nSide-e's compilation targets
    Renamed all of nSide-e's compilation targets to begin with "ethos-"
    instead of "ui-" to avoid conflicts with nSide-t, which continues to use
    "ui-" instead of "tomoko-". This makes it easier to share compilation
    targets for emulators with the 2 user interfaces.

--- cc-ethos-console.bat
+++ cc-ethos-console.bat
@@ -5,12 +5,12 @@ mkdir "obj"
 rem Accuracy profile
 mkdir "obj\accuracy"
 move "obj\accuracy\sfc-*.o" "obj"
-move "obj\accuracy\ui-ethos.o" "obj"
-move "obj\accuracy\ui-settings.o" "obj"
+move "obj\accuracy\ethos-ethos.o" "obj"
+move "obj\accuracy\ethos-settings.o" "obj"
 mingw32-make -j4 target=nSide-e profile=accuracy console=true
 move "obj\sfc-*.o" "obj\accuracy"
-move "obj\ui-ethos.o" "obj\accuracy"
-move "obj\ui-settings.o" "obj\accuracy"
+move "obj\ethos-ethos.o" "obj\accuracy"
+move "obj\ethos-settings.o" "obj\accuracy"
 if not exist "out\nSide-e.exe" (pause)
 
 @echo on
--- cc-ethos.bat
+++ cc-ethos.bat
@@ -5,23 +5,23 @@ mkdir "obj"
 rem Accuracy profile
 mkdir "obj\accuracy"
 move "obj\accuracy\sfc-*.o" "obj"
-move "obj\accuracy\ui-ethos.o" "obj"
-move "obj\accuracy\ui-settings.o" "obj"
+move "obj\accuracy\ethos-ethos.o" "obj"
+move "obj\accuracy\ethos-settings.o" "obj"
 mingw32-make -j4 target=nSide-e profile=accuracy
 move "obj\sfc-*.o" "obj\accuracy"
-move "obj\ui-ethos.o" "obj\accuracy"
-move "obj\ui-settings.o" "obj\accuracy"
+move "obj\ethos-ethos.o" "obj\accuracy"
+move "obj\ethos-settings.o" "obj\accuracy"
 if not exist "out\nSide-e.exe" (pause)
 
 rem Balanced profile
 mkdir "obj\balanced"
 move "obj\balanced\sfc-*.o" "obj"
-move "obj\balanced\ui-ethos.o" "obj"
-move "obj\balanced\ui-settings.o" "obj"
+move "obj\balanced\ethos-ethos.o" "obj"
+move "obj\balanced\ethos-settings.o" "obj"
 mingw32-make -j4 target=nSide-e profile=balanced name=nSide-e-balanced
 move "obj\sfc-*.o" "obj\balanced"
-move "obj\ui-ethos.o" "obj\balanced"
-move "obj\ui-settings.o" "obj\balanced"
+move "obj\ethos-ethos.o" "obj\balanced"
+move "obj\ethos-settings.o" "obj\balanced"
 if not exist "out\nSide-e-balanced.exe" (pause)
 
 @echo on
