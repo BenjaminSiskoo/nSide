46c9c5f630cb95bea56fe82a6a29b8fe4e5d0523
hex-usr <hex-usr@users.noreply.github.com>
2016-02-23 08:46:10 -0800

    Four Score regression fix
    The Four Score was broken by a recent commit. This commit fixes it.

--- nSide/fc/controller/fourscore/fourscore.cpp
+++ nSide/fc/controller/fourscore/fourscore.cpp
@@ -5,28 +5,34 @@ FourScore::FourScore(uint port) : Controller(port, (uint)Device::ID::FourScore) 
 
 auto FourScore::data() -> uint5 {
   if(counter >= 24) return 1;
-  if(latched) return poll(0);
-  uint5 data = 0;
+  if(latched) return poll(A);
 
-  if(counter >= 16) data = counter == (19 - port);
   //note: D-pad physically prevents up+down and left+right from being pressed at the same time
-  else switch(counter++) {
-  case  0: return aL << 1;
-  case  1: return bL << 1;
-  case  2: return selectL << 1;
-  case  3: return startL << 1;
-  case  4: return (upL & !downL) << 1;
-  case  5: return (downL & !upL) << 1;
-  case  6: return (leftL & !rightL) << 1;
-  case  7: return (rightL & !leftL) << 1;
-  case  8: return aR << 1;
-  case  9: return bR << 1;
-  case 10: return selectR << 1;
-  case 11: return startR << 1;
-  case 12: return (upR & !downR) << 1;
-  case 13: return (downR & !upR) << 1;
-  case 14: return (leftR & !rightR) << 1;
-  case 15: return (rightR & !leftR) << 1;
+  switch(counter++) {
+  case  0: return aL;
+  case  1: return bL;
+  case  2: return selectL;
+  case  3: return startL;
+  case  4: return (upL & !downL);
+  case  5: return (downL & !upL);
+  case  6: return (leftL & !rightL);
+  case  7: return (rightL & !leftL);
+  case  8: return aR;
+  case  9: return bR;
+  case 10: return selectR;
+  case 11: return startR;
+  case 12: return (upR & !downR);
+  case 13: return (downR & !upR);
+  case 14: return (leftR & !rightR);
+  case 15: return (rightR & !leftR);
+  case 16: return 0;
+  case 17: return 0;
+  case 18: return port == 1;
+  case 19: return port == 0;
+  case 20: return 0;
+  case 21: return 0;
+  case 22: return 0;
+  case 23: return 0;
   }
 }
 
