0e66ae4a80566bd6d282b47c0a5e7713e6fc6483
hex-usr <hex-usr@users.noreply.github.com>
2017-02-16 10:11:05 -0800

    Family Trainer protocol fix
    The Family Trainer uses a completely different protocol compared to the
    Power Pad. Originally, testing the Family Trainer games resulted in
    false positives because, by coincidence, buttons 1 and 2 respond in
    similar ways on both devices. This has now been fixed.

--- nSide/fc/expansion/family-trainer/family-trainer.cpp
+++ nSide/fc/expansion/family-trainer/family-trainer.cpp
@@ -1,6 +1,4 @@
 FamilyTrainer::FamilyTrainer() {
-  latched = 0;
-  counter = 0;
 }
 
 auto FamilyTrainer::data1() -> bool {
@@ -8,39 +6,32 @@ auto FamilyTrainer::data1() -> bool {
 }
 
 auto FamilyTrainer::data2() -> uint5 {
-  if(counter >= 8) return 0x18;
-  if(latched == 1) return (platform->inputPoll(ID::Port::Expansion, ID::Device::FamilyTrainer, 1) << 1) | (platform->inputPoll(ID::Port::Expansion, ID::Device::FamilyTrainer, 3) << 2);
-
-  switch(counter++) {
-  case 0: return (b2  << 3) | (b4  << 4);
-  case 1: return (b1  << 3) | (b3  << 4);
-  case 2: return (b5  << 3) | (b12 << 4);
-  case 3: return (b9  << 3) | (b8  << 4);
-  case 4: return (b6  << 3) | (1   << 4);
-  case 5: return (b10 << 3) | (1   << 4);
-  case 6: return (b11 << 3) | (1   << 4);
-  case 7: return (b7  << 3) | (1   << 4);
+  uint5 result = 0x00;
+  #define port ID::Port::Expansion
+  #define device ID::Device::FamilyTrainer
+  if(!mask.bit(2)) {
+    result.bit(4) |= !platform->inputPoll(port, device,  0);
+    result.bit(3) |= !platform->inputPoll(port, device,  1);
+    result.bit(2) |= !platform->inputPoll(port, device,  2);
+    result.bit(1) |= !platform->inputPoll(port, device,  3);
   }
-  unreachable;
+  if(!mask.bit(1)) {
+    result.bit(4) |= !platform->inputPoll(port, device,  4);
+    result.bit(3) |= !platform->inputPoll(port, device,  5);
+    result.bit(2) |= !platform->inputPoll(port, device,  6);
+    result.bit(1) |= !platform->inputPoll(port, device,  7);
+  }
+  if(!mask.bit(0)) {
+    result.bit(4) |= !platform->inputPoll(port, device,  8);
+    result.bit(3) |= !platform->inputPoll(port, device,  9);
+    result.bit(2) |= !platform->inputPoll(port, device, 10);
+    result.bit(1) |= !platform->inputPoll(port, device, 11);
+  }
+  #undef port
+  #undef device
+  return result;
 }
 
 auto FamilyTrainer::write(uint3 data) -> void {
-  if(latched == data.bit(0)) return;
-  latched = data.bit(0);
-  counter = 0;
-
-  if(latched == 0) {
-    b1  = platform->inputPoll(ID::Port::Expansion, ID::Device::FamilyTrainer,  0);
-    b2  = platform->inputPoll(ID::Port::Expansion, ID::Device::FamilyTrainer,  1);
-    b3  = platform->inputPoll(ID::Port::Expansion, ID::Device::FamilyTrainer,  2);
-    b4  = platform->inputPoll(ID::Port::Expansion, ID::Device::FamilyTrainer,  3);
-    b5  = platform->inputPoll(ID::Port::Expansion, ID::Device::FamilyTrainer,  4);
-    b6  = platform->inputPoll(ID::Port::Expansion, ID::Device::FamilyTrainer,  5);
-    b7  = platform->inputPoll(ID::Port::Expansion, ID::Device::FamilyTrainer,  6);
-    b8  = platform->inputPoll(ID::Port::Expansion, ID::Device::FamilyTrainer,  7);
-    b9  = platform->inputPoll(ID::Port::Expansion, ID::Device::FamilyTrainer,  8);
-    b10 = platform->inputPoll(ID::Port::Expansion, ID::Device::FamilyTrainer,  9);
-    b11 = platform->inputPoll(ID::Port::Expansion, ID::Device::FamilyTrainer, 10);
-    b12 = platform->inputPoll(ID::Port::Expansion, ID::Device::FamilyTrainer, 11);
-  }
+  mask = data;
 }
--- nSide/fc/expansion/family-trainer/family-trainer.hpp
+++ nSide/fc/expansion/family-trainer/family-trainer.hpp
@@ -6,10 +6,5 @@ struct FamilyTrainer : Expansion {
   auto write(uint3 data) -> void;
 
 private:
-  bool latched;
-  uint counter;
-
-  boolean b1, b2,  b3,  b4;
-  boolean b5, b6,  b7,  b8;
-  boolean b9, b10, b11, b12;
+  uint3 mask;
 };
